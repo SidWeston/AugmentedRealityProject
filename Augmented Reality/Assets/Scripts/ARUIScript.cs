a04dbbe4d8053e35076b9a55387c0c3a8acf457d
SidWeston <60944006+SidWeston@users.noreply.github.com>
2022-05-05 17:21:48 +0100

    add menu and ui

---
+++ Augmented Reality/Assets/Scripts/ARUIScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ARUIScript : MonoBehaviour
+{
+    public Button backButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //bind function to button
+        backButton.onClick.AddListener(OnBackButton);
+    }
+
+    public void OnBackButton()
+    {
+        //load back to the menu screen on button pressed
+        //assume menu scene will always have the index 0
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Augmented Reality/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public Button cameraButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //bind function to button
+        cameraButton.onClick.AddListener(OnCameraButton);
+    }
+
+    public void OnCameraButton()
+    {
+        //load ar camera scene
+        //assume scene will always have index 1 in build settings
+        SceneManager.LoadScene(1);
+    }
+}
