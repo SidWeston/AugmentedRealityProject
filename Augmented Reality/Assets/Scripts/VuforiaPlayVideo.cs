52f261e6d4bafa669c8d14ab59b372af771ff69a
SidWeston <60944006+SidWeston@users.noreply.github.com>
2022-05-05 17:35:21 +0100

    video play fix?

---
+++ Augmented Reality/Assets/Scripts/VuforiaPlayVideo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.Video;
+
+public class VuforiaPlayVideo : MonoBehaviour
+{
+    public VideoPlayer videoToPlay;
+    public ImageTargetBehaviour imageBehaviour;
+
+    private void Update()
+    {
+        if (imageBehaviour.CurrentStatus == TrackableBehaviour.Status.DETECTED && !videoToPlay.isPlaying)
+        {
+            PlayVideo();
+        }
+        else if (imageBehaviour.CurrentStatus != TrackableBehaviour.Status.DETECTED && videoToPlay.isPlaying)
+        {
+            StopVideo();
+        }
+    }
+
+    private void PlayVideo()
+    {
+        videoToPlay.Play();
+    }
+
+    private void StopVideo()
+    {
+        videoToPlay.Stop();
+    }
+}
